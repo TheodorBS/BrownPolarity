47ce7ac7d47dca6f87ea68f7a487def6385a664b
TheodorBS <theodor22@ru.is>
2025-02-24 09:59:39 +0000

    wa

--- AGDDPlatformer-Polarity-Base-master/Assets/Scripts/Spikes.cs
+++ AGDDPlatformer-Polarity-Base-master/Assets/Scripts/Spikes.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace AGDDPlatformer
+{
 public class Spikes : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
@@ -9,6 +11,8 @@ public class Spikes : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             Debug.Log("wawawa");
+
         }
     }
+}
 }
