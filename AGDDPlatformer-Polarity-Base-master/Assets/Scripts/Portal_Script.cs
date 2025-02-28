224d3a15014bf7e1bf1fe36cfda60b0b21dd7f28
TheodorBS <theodor22@ru.is>
2025-02-28 13:37:31 +0000

    portal fix

--- AGDDPlatformer-Polarity-Base-master/Assets/Scripts/Portal_Script.cs
+++ AGDDPlatformer-Polarity-Base-master/Assets/Scripts/Portal_Script.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Portal_Script : MonoBehaviour
@@ -6,20 +8,22 @@ public class Portal_Script : MonoBehaviour
     public Transform newPortalPos;
     public Vector2 offset = new Vector2(0.5f, 0);
     private static bool isTeleporting = false;
+    private static HashSet<string> teleportingPlayers = new HashSet<string>();
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!isTeleporting && other.gameObject.layer == LayerMask.NameToLayer("Player"))
-        {
-            StartCoroutine(Teleport(other));
-        }
+        string playerName = other.gameObject.name;
+        if (!teleportingPlayers.Contains(playerName))
+            {
+                StartCoroutine(Teleport(other.gameObject, playerName));
+            }
     }
 
-    private IEnumerator Teleport(Collider2D player)
+    private IEnumerator Teleport(GameObject player, string playerName)
     {
-        isTeleporting = true;
+        teleportingPlayers.Add(playerName); 
         player.transform.position = (Vector2)newPortalPos.position + offset;
-        yield return new WaitForSeconds(0.5f);
-        isTeleporting = false;
+        yield return new WaitForSeconds(1f);
+        teleportingPlayers.Remove(playerName);
     }
 }
